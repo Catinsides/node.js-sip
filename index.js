32a2e89047afa656081b538f79ead5e34332949c
catinsides <1121409602@qq.com>
2024-11-25 15:23:31 +0800

    fix: fix module resolve

---
+++ index.js
@@ -0,0 +1,17 @@
+const Builder = require('./Builder.js');
+const Parser = require('./Parser.js');
+const SIPMessage = require('./SIPMessage.js');
+const SDPParser = require('./SDPParser.js');
+const Dialog = require('./Dialog.js');
+const Transaction = require('./Transaction.js');
+const SIP = require('./SIP.js');
+
+module.exports = {
+  Builder,
+  Parser,
+  SIPMessage,
+  SDPParser,
+  Dialog,
+  Transaction,
+  SIP
+};
